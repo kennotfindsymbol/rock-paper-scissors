3a48e695fa6c9cd772e7d8c2acec7ad222e3de0b
kennotfindsymbol <kennotfindsymbol@gmail.com>
2024-01-19 13:35:24 +0800

    Create index.html and script.js

--- README.md
+++ README.md
@@ -6,4 +6,14 @@ This is a rock paper scissors game built with HTML, CSS, and Javascript.
 
 This is my project for [The Odin Project](https://www.theodinproject.com/lessons/foundations-landing-page).
 
+It is a 5-round game. If you win a round you get 3 points. If you tie with the computer you get 1 point. If you lose no point.
+
+## How to play
+
+- Press F12 to open the console.
+- type game() in the console.
+- Type your choice between 'Rock' or 'Paper' or 'Scissors' (case does not matter).
+- See if you win, tie or lose this round.
+- After 5 rounds, you will see your total score.
+
 ### View and play here: <link>
---
+++ script.js
@@ -0,0 +1,50 @@
+function getComputerChoice() {
+    items = ['Rock', 'Paper', 'Scissors'];
+    return items[Math.floor(Math.random() * items.length)]
+}
+
+function capitalize(s) {
+    if (typeof s != "string" || s.length === 0) {
+        return s;
+    }
+    return s[0].toUpperCase() + s.slice(1).toLowerCase();
+}
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = capitalize(playerSelection);
+    computerSelection = capitalize(computerSelection);
+    if (playerSelection === computerSelection) {
+        return `Tie! Both chose ${playerSelection}`;
+    }
+    if (playerSelection === 'Rock' && computerSelection === 'Paper'
+        || playerSelection === 'Paper' && computerSelection === 'Scissors'
+        || playerSelection === 'Scissors' && computerSelection === 'Rock') {
+        return `You Lose! ${computerSelection} beats ${playerSelection}`;
+    }
+    if (playerSelection === 'Rock' && computerSelection === 'Scissors'
+        || playerSelection === 'Paper' && computerSelection === 'Rock'
+        || playerSelection === 'Scissors' && computerSelection === 'Paper') {
+        return `You Win! ${playerSelection} beats ${computerSelection}`;
+    }
+}
+
+function game() {
+    let score = 0;
+    let win = 0, tie = 0, lose = 0;
+    for (let i = 0; i < 5; i++){
+        const playerSelection = prompt(`Round${i + 1} Choice?`)
+        const computerSelection = getComputerChoice();
+        const msg = playRound(playerSelection, computerSelection);
+        if (msg.includes('Win')){
+            win++;
+            score += 3;
+        } else if (msg.includes('Tie')){
+            tie++;
+            score += 1;
+        } else {
+            lose++;
+        }
+        console.log(msg);
+    }
+    console.log(`You have ${win} wins, ${tie} ties, and ${lose} loses\nYour total score is ${score}`);
+}
